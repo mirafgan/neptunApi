70cb19c730218d0888f6645f697d37b30e5de656
mirafgan <mirefqan1999@gmail.com>
2024-04-08 16:47:57 +0400

    neptun

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,7 @@ const router = express.Router();
 router.get("/catalog", (req, res) => {
     res.send({ data: megamenu })
 })
+router.get('/', (req, res) => {
+    res.send("salam men neptun api")
+})
 module.exports = router
